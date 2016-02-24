e9cf0b7a49c119bad2352a7500900c6e3da11a95
David Su <david.d.su@gmail.com>
2016-02-24 16:18:26 +0100

    implement difficulty calc, rename syncopation calculation to more general calculate_value_for_bin

--- parser.py
+++ parser.py
@@ -114,6 +114,16 @@ duration_to_note_attrs = {
     1024: {'type': 'whole'}
 }
 
+# Definitions for difficulty values relative to original transcription
+# (so D(original_transcription) = 1)
+# 
+
+
+# Calculate overall difficulty of a bin based on d(ensity), k(eith), and c(oordination) as well as corresponding w(eights)
+def calculate_difficulty(d, k, c, w={'d': 0.33, 'k': 0.34, 'c': 0.33}):
+    return ( (d*w['d']) + (k*w['k']) + (c*w['c']) )
+
+
 # Augmentation of a note, given several params
 def parse_note(note, prev_note, duration_min, duration_left):
     print '{} left'.format(duration_left)
@@ -173,7 +183,7 @@ def parse_note(note, prev_note, duration_min, duration_left):
         else:
             note.pop('time-modification', None)
 
-        note.pop('default-x', None)
+        note.pop('@default-x', None)
 
         print '{}, {}'.format(note['duration'], note_attrs)
     return note
@@ -193,6 +203,8 @@ def debug_unparse(data, label):
     # return
 
 def to_note_name(note):
+    if 'unpitched' not in note:
+        return 'B#0'
     unpitched = note['unpitched']
     return '{}{}'.format(unpitched['display-step'], unpitched['display-octave'])
 
@@ -211,7 +223,7 @@ def note_to_rest(note):
     return rest
 
 # Determine the syncopation value of a beat (bin) via one of several methods
-def syncopation_value_beat(beat, method, bin_size, bin_divisions=4, max_bin_granularity=32):
+def calculate_value_for_bin(bin, method, bin_size, bin_divisions=4, max_bin_granularity=32):
     value = 0
     granularity = max_bin_granularity / bin_divisions
 
@@ -219,13 +231,13 @@ def syncopation_value_beat(beat, method, bin_size, bin_divisions=4, max_bin_gran
     # - essentially calculates onsets over duration
     # - make sure max_bin_granularity > the max number of possible onsets you might have, otherwise value will be greater than 1
     if method == 'DENSITY':
-        total_bin_duration = get_total_bin_duration(beat)
+        total_bin_duration = get_total_bin_duration(bin)
         if total_bin_duration > 0:
             # value = float(len(beat)) / total_bin_duration
             # value = value * bin_size / granularity
 
             # simplified calculation that accounts for rests
-            value = float(len([note for note in beat if 'rest' not in note])) / granularity
+            value = float(len([note for note in bin if 'rest' not in note])) / granularity
     
 
     # Keith's Measure:
@@ -260,14 +272,57 @@ def syncopation_value_beat(beat, method, bin_size, bin_divisions=4, max_bin_gran
                     cur_value = 1
             else:
                 if cur_note_on_beat:
-                    cur_value = 3
+                    cur_value = 2
                 else:
-                    cur_value = 4
+                    cur_value = 3
 
-            cur_value = float(cur_value) / 4 # scale it so value < 1
+            # normalize cur_value
+            cur_value = float(cur_value) / 3
+
+            # add to total value
             value += cur_value / len(bin)
 
+            # increment current duration
             cur_duration += note_duration
+    
+    elif method == 'COORDINATION':
+        # print bin[0].keys()
+        default_xs = [note['@default-x'] for note in bin]
+        print default_xs
+        num_notes = len(bin)
+        value = 0
+        max_simultaneous_limbs = 4 # RH, RF, LH, LF
+
+        for ni, note in enumerate(bin):
+            cur_value = 0
+            cur_note_name = to_note_name(note)
+            next_note_name = cur_note_name
+            prev_note_name = cur_note_name
+
+            simultaneous_onsets = len(filter(lambda x: x == note['@default-x'], default_xs))
+            # print simultaneous_onsets
+
+            if ni < num_notes - 1:
+                next_note = bin[ni+1]
+                next_note_name = to_note_name(next_note)
+            if ni > 0:
+                prev_note = bin[ni-1]
+                prev_note_name = to_note_name(prev_note)
+
+            # score according to contextual note interdependence difficulty (CNID) measure
+            if cur_note_name != next_note_name:
+                cur_value += 1
+            if cur_note_name != prev_note_name:
+                cur_value += 1
+            if prev_note_name != next_note_name and cur_value > 0:
+                cur_value += 1
+            cur_value += simultaneous_onsets / max_simultaneous_limbs
+
+            # normalize score
+            cur_value = float(cur_value) / 4
+
+            # add to total value
+            value += cur_value / num_notes
 
 
     return value
@@ -278,6 +333,8 @@ def get_total_bin_duration(bin):
 if __name__ == '__main__':
     score_xml_in_path = sys.argv[1]
     score_xml_out_path = sys.argv[2]
+    difficulty_gradient = sys.argv[3]
+    minimum_difficulty = sys.argv[4]
 
     score_xml = ''
 
@@ -329,13 +386,16 @@ if __name__ == '__main__':
         # output analysis to validate correct binning
         for bi, bin in enumerate(bins):
             total_duration = get_total_bin_duration(bin)
-            sync_density = syncopation_value_beat(bin, 'DENSITY', bin_duration, bin_divisions)
-            sync_keith = syncopation_value_beat(bin, 'KEITH', bin_duration, bin_divisions)
-            print 'beat {}: {} notes, total_duration={}, sync_density={}, sync_keith={}'.format(bi+1, len(bin), total_duration, sync_density, sync_keith)
+            bin_density = calculate_value_for_bin(bin, 'DENSITY', bin_duration, bin_divisions)
+            bin_keith = calculate_value_for_bin(bin, 'KEITH', bin_duration, bin_divisions)
+            bin_coordination = calculate_value_for_bin(bin, 'COORDINATION', bin_duration, bin_divisions)
+            difficulty = calculate_difficulty(bin_density, bin_keith, bin_coordination)
+            print 'measure {} beat {}: {} notes, total_duration={}, bin_density={}, bin_keith={}, bin_coordination={}, difficulty={}'.format(mi+1, bi+1, len(bin), total_duration, bin_density, bin_keith, bin_coordination, difficulty)
 
-            
 
+        # create new phrase
         # for bi, bin in enumerate(bins):
+        #     bin = adjust_bin()
 
 
         # # iterate through notes and adjust durations (or delete note) using parse_note()
